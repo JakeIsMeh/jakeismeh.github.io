2ab8d2c0489af36af1b77282adb68665fbb6da3c
JakeIsMeh <jakeismeh1@gmail.com>
2023-03-20 13:01:25 +0800

    a

---
+++ src/routes/(layouted)/about/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import IconExtLink from 'virtual:icons/feather/external-link';
+</script>
+
+<h1>About</h1>
+<p>
+    Detail-oriented computer science student with a multi-faceted skillset and interests
+    spanning multiple domains. On a mission to reverse Wirth’s law and hoping to make an
+    impact through software engineering.
+</p>
+
+<style lang="scss">
+</style>
