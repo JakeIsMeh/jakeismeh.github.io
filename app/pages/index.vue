e0533459ef226297facce3a32cf1e83df6c0532f
JakeIsMeh <jakeismeh1@gmail.com>
2024-07-06 00:47:59 +0800

    fix css

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -83,7 +83,7 @@ a.nav-link:focus {
 .pfp {
     width: 16rem;
     height: 16rem;
-    border: .0625rem solid;
+    border: .125rem solid;
     border-radius: $radius;
     background-color: var(--t-pink);
 }
